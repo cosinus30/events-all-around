d890d514d14c68fbbe86c426b5b0f3801bcd2a70
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2021-12-14 16:55:03 +0300

    feat: add all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,13 @@
+import EventList from "../../components/events/event-list";
+import { getAllEvents } from "../../dummy-data";
+
 function EventsPage() {
+    const allEvents = getAllEvents()
+
     return (
         <div>
-            <h1>Events Page!</h1>
+            <h1>All Events</h1>
+            <EventList items={allEvents}/>
         </div>
     )
 }
