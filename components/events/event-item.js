3312df23aae8f2507ea655b2647a58b62867eef8
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2021-12-14 16:17:43 +0300

    chore(style): add icons to the event card

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import Button from '../ui/button';
 import styles from './event-item.module.css';
+import DateIcon from '../icons/date-icon';
+import AdressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 
 function EventItem({ data }) {
 	const { title, image, date, location, id } = data;
@@ -13,19 +17,26 @@ function EventItem({ data }) {
 
 	return (
 		<li className={styles.card}>
-            <img src={'/' + image} alt={title}/>
+			<img src={'/' + image} alt={title} />
 			<div className={styles.content}>
 				<div className={styles.summary}>
 					<h2>{title}</h2>
 					<div className={styles.date}>
+						<DateIcon />
 						<time>{formattedDate}</time>
 					</div>
 					<div className={styles.address}>
+						<AdressIcon />
 						<address>{location}</address>
 					</div>
 				</div>
 				<div className={styles.actions}>
-					<Link href={`events/${id}`}>Explore Event</Link>
+					<Button link={`events/${id}`}>
+						<span>Explore Event</span>
+						<span className={styles.icon}>
+							<ArrowRightIcon />
+						</span>
+					</Button>
 				</div>
 			</div>
 		</li>
---
+++ components/ui/button.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import styles from './button.module.css'
+
+function Button({children, link}){
+    return (
+        <Link href={link}>
+            <a className={styles.btn}>
+                {children}
+            </a>
+        </Link>
+    )
+
+}
+
+export default Button;
