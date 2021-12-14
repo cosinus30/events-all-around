c0b1d567b2f71c7f4c4e59906b90049cd1223195
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2021-12-14 14:50:05 +0300

    chore: set up initial structure

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
-export default function Home() {
-  return (
-    <div>
+import Link from "next/link";
 
-    </div>
-  )
+export default function Home() {
+	return (
+		<div>
+			<h1>Home Page!</h1>
+			<ul>
+				<li>
+					<Link href="/events">Events</Link>
+				</li>
+        <li>
+          <Link href="/events/1">Event Detail</Link>
+        </li>
+        <li>
+          <Link href="/events/2020/12/14">Event Filter</Link>
+        </li>
+			</ul>
+		</div>
+	);
 }
