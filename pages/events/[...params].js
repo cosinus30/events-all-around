20dba71ef51114a623a560967bfd00eebea9e116
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2021-12-15 22:23:03 +0300

    feat: add event search capability

--- pages/events/[...params].js
+++ pages/events/[...params].js
@@ -1,7 +1,32 @@
+import { useRouter } from 'next/router';
+import EventList from '../../components/events/event-list';
+import { getFilteredEvents } from '../../dummy-data';
+
 function FilteredEventsPage() {
+	const router = useRouter();
+	const [year, month] = router.query.params;
+
+	if (!year || !month) {
+		return <p className="center">Loading...</p>;
+	}
+	if (isNaN(+year) || isNaN(+month)) {
+		return <p className="center">Invalid year or month</p>;
+	}
+
+	const filteredEvents = getFilteredEvents({ year: +year, month: +month });
+
+	if (!filteredEvents || filteredEvents.length === 0) {
+		return (
+			<p className="center">
+				No events found for {year}/{month}
+			</p>
+		);
+	}
+
 	return (
 		<div>
 			<h1>Filtered Events Page!</h1>
+			<EventList items={filteredEvents} />
 		</div>
 	);
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,21 @@
+import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../dummy-data";
 
 function EventsPage() {
     const allEvents = getAllEvents()
+    const router = useRouter()
+
+    function findEventsHandler(year, month){
+        const fullPath = `/events/${year}/${month}`
+        router.push(fullPath)
+    }
 
     return (
         <div>
             <h1>All Events</h1>
+            <EventsSearch onSearch={findEventsHandler}/>
             <EventList items={allEvents}/>
         </div>
     )
