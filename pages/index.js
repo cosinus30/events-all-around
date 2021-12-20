19723b419a7aba37a9fc35d9562bbb7133770603
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2021-12-20 18:22:16 +0300

    feat: ISG for featured events

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
 import Link from 'next/link';
 import EventList from '../components/events/event-list';
-import { getFeaturedEvents } from '../dummy-data';
+import { useFeaturedEvents } from '../hooks/data-hooks';
 
-export default function Home() {
-	const featuredEvents = getFeaturedEvents();
+export default function Home({featuredEvents}) {
 
 	return (
 		<div>
 			<EventList items={featuredEvents} />
 		</div>
 	);
+}
+
+export async function getStaticProps() {
+	const featuredEvents = await useFeaturedEvents();
+	console.log('Hey: ', featuredEvents)
+
+	return {
+		props: {
+			featuredEvents: featuredEvents
+		},
+		revalidate: 3600
+	}
 }
