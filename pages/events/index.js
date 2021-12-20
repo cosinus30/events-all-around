86f1bec518df5d653d443482f73f03d0cf35d603
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2021-12-20 18:22:40 +0300

    feat: ISG for all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,10 @@
 import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
-import { getAllEvents } from "../../dummy-data";
+import { useEvents } from "../../hooks/data-hooks";
 
-function EventsPage() {
-    const allEvents = getAllEvents()
+function EventsPage({allEvents}) {
     const router = useRouter()
-
     function findEventsHandler(year, month){
         const fullPath = `/events/${year}/${month}`
         router.push(fullPath)
@@ -19,6 +17,18 @@ function EventsPage() {
             <EventList items={allEvents}/>
         </div>
     )
+}
+
+export async function getStaticProps(){
+    const allEvents = await useEvents()
+
+    return {
+        props: {
+            allEvents: allEvents
+        }, 
+        revalidate: 300
+    }
+
 }
 
 export default EventsPage;
