090db32623355cdd4699834dc516ddd151257efc
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2021-12-23 01:56:56 +0300

    chore: add generic metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
+import Head from 'next/head';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
-			{' '}
-			<Component {...pageProps} />{' '}
+			<Head>
+				<title>Events All Around</title>
+				<meta name="description" content="Find a lot of great events that make you better" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+			<Component {...pageProps} />
 		</Layout>
 	);
 }
